e9f562968b73f199add806b55e53570f0daf23cb
Nurcahyo132 <61648714+Nurcahyo132@users.noreply.github.com>
2020-10-14 13:11:02 +0700

    State & Component Lifecycle

--- Tugas-9/my-app/src/App.js
+++ Tugas-9/my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Tugas9 from './Tugas-9/Tugas9'
 import Table from './Tugas-10/Table'
+import Timer from './Tugas-11/Timer'
 import './App.css';
 
 function App(){
@@ -8,6 +9,7 @@ function App(){
     <div className="">
       <Tugas9 />
       <Table />
+      <Timer />
   
     </div>
   )
